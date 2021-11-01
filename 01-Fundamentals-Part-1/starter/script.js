154840af40f930db0f831c9fb844476eb922c81b
arnameti <arnameti@gmail.com>
2021-11-01 18:08:29 +0100

    Tearnary Operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -462,3 +462,38 @@ switch(day) {
     console.log('Not a valid day!');
 }
 */
+
+// ----------------------------------------------------------------------------------------------------
+// #################### Statements and Expressions
+
+/*
+3 + 4
+1991
+true && flase && !false
+
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+*/
+
+// ----------------------------------------------------------------------------------------------------
+// #################### Conditional (Ternary) Operator
+
+/*
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine';
+} else {
+  drink2 = 'water';
+}
+
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+*/
