c0abb0c61d6f720e9823fc46db0727f61a107de7
arnameti <arnameti@gmail.com>
2021-11-01 10:56:52 +0100

    let, const and var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -69,3 +69,24 @@ console.log(typeof year);
 
 console.log(typeof null);
 */
+
+// ----------------------------------------------------------------------------------------------------
+// #################### let, const and var
+
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+*/
+
+
