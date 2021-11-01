30c6ae45822c80f28404c04ab0e3c92c51095f2d
arnameti <arnameti@gmail.com>
2021-11-01 17:42:47 +0100

    switch statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -432,3 +432,33 @@ if (avarageDolphins > avarageKoalas && avarageDolphins >= 100) {
   console.log('nobody has won');
 }
 */
+
+// ----------------------------------------------------------------------------------------------------
+// #################### The switch Statemement
+
+/*
+const day = 'thursday';
+
+switch(day) {
+  case 'monday': // day === monday
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
+*/
