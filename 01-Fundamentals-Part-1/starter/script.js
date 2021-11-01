2b17bb17eb98249cdef7fa95dd114f395508ec0a
arnameti <arnameti@gmail.com>
2021-11-01 10:24:21 +0100

    variables and values

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,44 @@
+/*
+let js = "amazing";
+if (js === "amazing") alert("Javascript is FUN");
+
+console.log(40 + 8 + 23 - 10);
+*/
+
+// ----------------------------------------------------------------------------------------------------
+// #################### Values and Variables
+
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+let first = "Jonas";
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let jonsd&matilda = 'JM';
+// let new = 27;
+// let function = 27;
+// _ and $ are the only two symbols that are availible.
+
+let Person = "Jonas";
+// Do not start a variable name with an uppercase letter.
+
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+*/
