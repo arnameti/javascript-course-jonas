dde317b8f0a551de6f2fde454d8b4bd93df3de1c
arnameti <arnameti@gmail.com>
2021-11-01 15:35:17 +0100

    Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -245,3 +245,30 @@ if (marksBMI > johnsBMI) {
   console.log(`John's BMI (${johnsBMI}) is higher than Marks's (${marksBMI})`);
 }
 */
+
+// ----------------------------------------------------------------------------------------------------
+// #################### Type Conversion and Coercion
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' + '10' + 3);
+console.log('23' * '2');
+console.log('23' > '18');
+
+let n = '1' + '1';
+n = n - 1;
+console.log(n);
+
+ 
