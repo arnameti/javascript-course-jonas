a0bb4a832b989f73f1c0e5ecf093c859177d0040
arnameti <arnameti@gmail.com>
2021-11-02 18:03:36 +0100

    Introduction to objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -217,6 +217,7 @@ if (friends.includes('Peter')) {
 // #################### Code Challange #6
 
 
+/*  
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
@@ -228,3 +229,41 @@ console.log(tips);
 
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(total);
+*/
+
+// ----------------------------------------------------------------------------------------------------
+// #################### Introduction to objects
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Stevens']
+}
+
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friendd');
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log('Wrong request! Choose between firstName, lastName, age, job and friend');
+// }
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman'
+console.log(jonas);
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and hies best friend is called ${jonas.friends[0]}`);
+
+
+
