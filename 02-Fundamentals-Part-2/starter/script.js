788e0a2c888df034107eb0b72b3ac4300225f983
arnameti <arnameti@gmail.com>
2021-11-02 16:57:11 +0100

    code challange #6

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -176,7 +176,7 @@ console.log(ages);
 // #################### Array Methods
 
 
-
+/*
 const friends = ["Michael", "Steven", "Peter"];
 const newLength = friends.push('Jay');
 
@@ -211,6 +211,20 @@ console.log(friends.includes(23));
 if (friends.includes('Peter')) {
   console.log ('You have a friend called Peter');
 }
+*/
+
+// ----------------------------------------------------------------------------------------------------
+// #################### Code Challange #6
 
 
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
 
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
