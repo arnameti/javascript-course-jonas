e33d59246e37b7e220e78a39110c197df317f3c0
arnameti <arnameti@gmail.com>
2021-11-02 15:25:38 +0100

    Introduction to Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -62,7 +62,6 @@ const age2 = calcAge2(1991);
 console.log(age1, age2);
 */
 
-
 // ----------------------------------------------------------------------------------------------------
 // #################### Arrow Functions
 
@@ -106,6 +105,7 @@ console.log(fruitProcessor(2, 3));
 // ----------------------------------------------------------------------------------------------------
 // #################### Code Challange #5
 
+/*
 const calcAvarage = (score1, score2, score3) => {
   const avarage = (score1 + score2 + score3) / 3;
   return avarage;
@@ -125,3 +125,48 @@ function checkWinner (avgDolphins, avgKoalas) {
 }
 
 checkWinner(scoreDolphins, scoreKoalas);
+*/
+
+// ----------------------------------------------------------------------------------------------------
+// #################### Introduction to Arrays
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+// console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const jonas = ['Jonas', 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+
+// Exercise
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
