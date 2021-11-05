ea1d889ad528b1566d7f3d750bf08eda0b0e27d3
arnameti <arnameti@gmail.com>
2021-11-05 17:06:13 +0100

    handling click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,24 @@
 'use strict';
+
+/*
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+// -----------------------------------------------------
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  // also if (guess === 0)
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+});
