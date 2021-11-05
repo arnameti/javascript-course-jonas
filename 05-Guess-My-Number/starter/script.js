9b25df5699b1b00246cdc9ed1e18ecc7cc5c3e2a
arnameti <arnameti@gmail.com>
2021-11-05 20:40:08 +0100

    manipulating css

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,20 +15,25 @@ console.log(document.querySelector('.guess').value);
 // -----------------------------------------------------
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
+  // When there is no input
   // also if (guess === 0)
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    // When guess is to high
   } else if (guess > secretNumber) {
-    if (score > 1) {
+    if (score >= 1) {
       document.querySelector('.message').textContent = 'Too  high!';
       score--;
       document.querySelector('.score').textContent = score;
@@ -36,6 +41,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game';
       document.querySelector('.score').textContent = 0;
     }
+    // When guess is to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too  low!';
