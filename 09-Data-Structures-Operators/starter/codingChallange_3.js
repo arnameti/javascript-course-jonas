cc0a7f12bc37b4a86931170ae5d551bb321d033e
arnameti <arnameti@gmail.com>
2021-12-03 09:42:01 +0100

    working with strings I

---
+++ 09-Data-Structures-Operators/starter/codingChallange_3.js
@@ -0,0 +1,40 @@
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+
+// 1
+console.log(gameEvents.values());
+const events = new Set (gameEvents.values());
+const eventsArray = [...new Set (gameEvents.values())];
+console.log(events);
+console.log(eventsArray);
+
+// 2
+gameEvents.delete(64);
+
+// 3
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4
+for (const [min, event] of gameEvents) {
+  const half = min <=45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
