0c40b5af2db8b999e79d8520e75015d153f0e7b9
arnameti <arnameti@gmail.com>
2021-12-03 10:40:44 +0100

    working with strings II

---
+++ 09-Data-Structures-Operators/starter/16_strings2.js
@@ -0,0 +1,59 @@
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log('jonas'.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnas'; //Jonas
+const passengerLower = passenger.toLocaleLowerCase();
+const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+
+//check email
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@JOnas.IO  \n';
+
+const lowerEmail = loginEmail.toLowerCase();
+console.log(lowerEmail);
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+// or
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement = 'All passengers cometo bording door 23. Boarding door 23!'
+
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boing'));
+console.log(plane.startsWith('Air'));
+
+if(plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function(items) {
+  const baggage = items.toLocaleLowerCase();
+  if(baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome abroad!');
+  }
+}
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection')
