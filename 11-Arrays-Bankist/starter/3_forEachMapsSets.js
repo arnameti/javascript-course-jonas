c9e8e13fb3f814041d655992b67229f447160f7e
arnameti <arnameti@gmail.com>
2022-01-08 10:47:39 +0100

    Bankist - Creating DOM Elements

--- 11-Arrays-Bankist/starter/3_forEachMapsSets.js
+++ 11-Arrays-Bankist/starter/3_forEachMapsSets.js
@@ -1,8 +1,21 @@
 'use strict'
 
+// Map
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
+
+currencies.forEach(function(value, key, map) {
+  console.log(`${key}: ${value}`);
+})
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function(value, _value, map) {
+  console.log(`${_value}: ${value}`);
+})
