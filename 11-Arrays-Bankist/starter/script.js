b3c4e23e31ec51d48618fca6c23822238e75ca95
arnameti <arnameti@gmail.com>
2022-01-08 20:10:58 +0100

    the magic of chaining

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,7 +61,6 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-
 // --------------------------------------------------------------
 const displayMovments = function (movements) {
   containerMovements.innerHTML = '';
@@ -74,7 +73,7 @@ const displayMovments = function (movements) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov} EUR</div>
       </div>
     `;
 
@@ -82,13 +81,32 @@ const displayMovments = function (movements) {
   });
 };
 displayMovments(account1.movements);
-
 // --------------------------------------------------------------
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance} EUR`;
 };
-calcDisplayBalance(account1.movements)
+calcDisplayBalance(account1.movements);
+// --------------------------------------------------------------
+const calcDisplaySummery = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes} EUR`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)} EUR`;
+
+  const interest =  movements
+      .filter(mov => mov > 0)
+      .map(deposit => (deposit * 1.2) / 100)
+      .filter(int => int >= 1)
+      .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest} EUR`;
+};
+calcDisplaySummery(account1.movements);
 // --------------------------------------------------------------
 const createUserNames = function (accs) {
   accs.forEach(function (acc) {
@@ -100,4 +118,3 @@ const createUserNames = function (accs) {
   });
 };
 createUserNames(accounts);
-
