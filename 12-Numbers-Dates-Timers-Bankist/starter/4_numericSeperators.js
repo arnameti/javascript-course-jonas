a21210a34006e40876927f0782de7bac06a772c0
arnameti <arnameti@gmail.com>
2022-01-11 13:00:29 +0100

    numericSeperators

---
+++ 12-Numbers-Dates-Timers-Bankist/starter/4_numericSeperators.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee = 15_00;
+const trasferFee = 1_500;
+
+const PI = 3.14_15
+console.log(PI);
+
+console.log(Number('230000'));
+console.log(Number('2300_00'));
+
+
+
+
+
+
+
+
