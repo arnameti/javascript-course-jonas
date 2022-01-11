d7e560d99cb37a321ca19dd04a8dfccb577cb759
arnameti <arnameti@gmail.com>
2022-01-11 12:38:36 +0100

    remainder operator

---
+++ 12-Numbers-Dates-Timers-Bankist/starter/3_remainderOperator.js
@@ -0,0 +1,24 @@
+'user strict';
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const evenOrOdd = number => (number % 2 === 0 ? 'even' : 'odd');
+console.log(evenOrOdd(9));
+console.log(evenOrOdd(8));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'yellowgreen';
+  });
+});
