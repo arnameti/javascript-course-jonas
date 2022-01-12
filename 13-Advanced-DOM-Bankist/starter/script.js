aa82e2f0fd1a9100d4773a09f4cfda0c2a57e33e
arnameti <arnameti@gmail.com>
2022-01-12 14:42:50 +0100

    implementing smoothScrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,7 +30,6 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
 const message = document.createElement('div');
 const header = document.querySelector('.header');
 
@@ -45,3 +44,33 @@ document
     // message.parentElement.removeChild(message);
     message.remove();
   });
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  // console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // Modern Way
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
