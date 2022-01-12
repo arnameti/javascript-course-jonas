2b84ee6d97c7efa0076412e82a5b747d7c0479bb
arnameti <arnameti@gmail.com>
2022-01-12 16:25:25 +0100

    eventPropagationInPractice

---
+++ 13-Advanced-DOM-Bankist/starter/5_bubblingAndCapturing.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// rgb(255, 255, 255)
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+
+// The event happens at the document-root and from there it travels to the target element 
+// and from there it bubbles up and bubbling up it means that basically it's as if the event
+// had also happened in all of the parent elements
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation(); 
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+ 
